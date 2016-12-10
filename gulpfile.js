94fe94504919a4502d33b27a78ff54297974dae0
yonas.mohammad <toro.yonas.mohammad@gmail.com>
2016-12-10 10:00:03 +0800

    comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-  // place code for your default task here
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('src/scss/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/assets/css'));
 });
+
+gulp.task('sass:watch', function () {
+  gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['sass']);
+});
+
+
+gulp.task('default', ['sass'])
